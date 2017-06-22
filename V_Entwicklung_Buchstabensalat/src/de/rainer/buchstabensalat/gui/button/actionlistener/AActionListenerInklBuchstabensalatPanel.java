6e27a38b37f438a6bbb03697284394b4afc17459
sunny <sunny@LAPTOP-MDAF799H.fritz.box>
2017-06-22 14:23:51 +0200

    ...

--- V_Entwicklung_Buchstabensalat/src/de/rainer/buchstabensalat/gui/button/actionlistener/AActionListenerInklBuchstabensalatPanel.java
+++ V_Entwicklung_Buchstabensalat/src/de/rainer/buchstabensalat/gui/button/actionlistener/AActionListenerInklBuchstabensalatPanel.java
@@ -1,12 +1,9 @@
 package de.rainer.buchstabensalat.gui.button.actionlistener;
 
-import java.awt.event.ActionListener;
-
-import de.rainer.buchstabensalat.data.IDataProvider;
 import de.rainer.buchstabensalat.gui.panel.BuchstabensalatPanel;
 
 public abstract class AActionListenerInklBuchstabensalatPanel implements
-		IDataProvider, ActionListener {
+		IActionListener {
 
 	private BuchstabensalatPanel bsp;
 
